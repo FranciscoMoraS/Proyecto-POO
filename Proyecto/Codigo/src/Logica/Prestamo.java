93ca4f9bfd9db648f187e0c2955fee7485c4fdd3
FranciscoMoraS <franmora1205@gmail.com>
2026-06-11 20:41:15 -0600

    Add files via upload

--- Proyecto/Codigo/src/Logica/Prestamo.java
+++ Proyecto/Codigo/src/Logica/Prestamo.java
@@ -1,5 +1,40 @@
 package Logica;
 
-public class Prestamo {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Prestamo {
+	private int ID;
+	private Persona persona;
+	private List<Item> items;
+	private Alerta alerta;
+	
+	
+	public Prestamo(Persona persona, int ID) {
+		this.ID=ID;
+		this.persona=persona;
+		items= new ArrayList<>();
+	}
+
+
+	public Persona getPersona() {
+		return persona;
+	}
+
+
+	public List<Item> getItems() {
+		return items;
+	}
+
+
+	public Alerta getAlerta() {
+		return alerta;
+	}
+	
+	public void agregarItem(Item item) {
+		//agregar comprobacion de item para saber si esta prestado o no
+		items.add(item);
+		//se le dice al item que pertenece al prestamo.
+	}
+	
 }
