4f97118741c70089d04f7c7c54e543a97400d44e
FranciscoMoraS <franmora1205@gmail.com>
2026-06-11 23:09:57 -0600

    Add files via upload

--- Proyecto/Codigo/src/Logica/Tipo.java
+++ Proyecto/Codigo/src/Logica/Tipo.java
@@ -1,5 +1,32 @@
 package Logica;
 
-public class Tipo {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Tipo {
+	private String Nombre;
+	private List<Item> lista;
+	
+	public Tipo(String nombre) {
+		this.Nombre=nombre;
+		this.lista= new ArrayList<>();
+	}
+
+	public String getNombre() {
+		return Nombre;
+	}
+
+	public void setNombre(String nombre) {
+		Nombre = nombre;
+	}
+
+	public List<Item> getLista() {
+		return lista;
+	}
+	public void agregarItem(Item agregar) {
+		lista.add(agregar);
+	}
+	public void quitarItem(Item quitar) {
+		lista.remove(quitar);
+	}
 }
